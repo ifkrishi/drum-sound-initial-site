8ddace7a3bae7c15e2fa81d2fde42022f42d0df4
ifkrishi <99007273+ifkrishi@users.noreply.github.com>
2022-07-06 13:06:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+document.querySelector(".w").addEventListener("click", function handleClick(){
+  new Audio("tom-1.mp3").play();
+}) ;
+document.querySelector(".a").addEventListener("click", function handleClick(){
+  new Audio("tom-2.mp3").play();
+}) ;
+document.querySelector(".s").addEventListener("click", function handleClick(){
+  new Audio("tom-3.mp3").play();
+}) ;
+document.querySelector(".d").addEventListener("click", function handleClick(){
+  new Audio("tom-4.mp3").play();
+}) ;
+document.querySelector(".j").addEventListener("click", function handleClick(){
+  new Audio("crash.mp3").play();
+}) ;
+document.querySelector(".k").addEventListener("click", function handleClick(){
+  new Audio("kick-bass.mp3").play();
+}) ;
+document.querySelector(".l").addEventListener("click", function handleClick(){
+  new Audio("snare.mp3").play();
+}) ;
